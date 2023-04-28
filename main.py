07128f821cf201ef4bd93e1ecc583557071c3252
Eric Bear <eric.lj.bear@gmail.com>
2023-04-28 07:29:40 -0600

    -added comments

--- main.py
+++ main.py
@@ -9,24 +9,32 @@ SCREEN_HEIGHT = 600
 # Player width and height of each segment
 snake_width = 20
 snake_height = 20
+# space between each snake segment
 segment_margin = 3
 
 class Snake(pygame.sprite.Sprite):
+    """ Player controlled Snake class """
+
+    # ---- Methods ----#
     def __init__(self, x, y) -> None:
+        """ Constructor Method """
         pygame.sprite.Sprite.__init__(self)
 
+        # Set the height and width of snake 
         self.image = pygame.Surface([snake_width, snake_height])
         self.image.fill(WHITE)
 
+        # Snake position
         self.rect = self.image.get_rect()    
-
         self.rect.x = x
         self.rect.y = y
 
+        # Snake speed vector
         self.change_x = 0
         self.change_y = 0
 
     def change_direction(self, dir):
+        # Changes the player direction
         match dir:
             case "left":
                 self.change_x = (snake_width + segment_margin) * -1
@@ -42,16 +50,20 @@ class Snake(pygame.sprite.Sprite):
                 self.change_y = (snake_height + segment_margin)
  
     def update(self):
-        """ Find a new position for the player"""
+        # Add the movement to the snake position
         self.rect.x += self.change_x
         self.rect.y += self.change_y
 
 def main():
+    # Initialize pygame
     pygame.init()
 
+    # Create an 800x600 sized screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # Set the title of the window
     pygame.display.set_caption("Snake by Northern Bear")
 
+    # Create a snake instance and add to snake group
     snake = Snake(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     snake_list = pygame.sprite.Group()
     snake_list.add(snake)
@@ -59,10 +71,12 @@ def main():
     clock = pygame.time.Clock()
     done = False
 
+    """ -----Main Program Loop----- """
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+            # Set the keyboard events for movement
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
                     snake.change_direction("left")
@@ -73,14 +87,18 @@ def main():
                 elif event.key == pygame.K_s:
                     snake.change_direction("down")
 
-        snake_list.update()                    
+        """ -----Game Logic----- """
+        snake_list.update()
 
+        """ -----Drawing Code----- """
+        # Clear the screen
         screen.fill(BLACK)
-
+        # Draw sprites
         snake_list.draw(screen)
-
+        # Flip the screen
         pygame.display.flip()
 
+        # set how many frames per second
         clock.tick(5)
 
     pygame.quit()
